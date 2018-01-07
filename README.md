4bb8bf5ed354ec1bfef15d8680b7332fcfd4777b
sjfricke <sjfricke@wisc.edu>
2018-01-07 16:57:33 -0600

    flag progress

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # go-nginx
 Personal script to setup nginx for a new domain
+
+## Download
+
+- `go get github.com/sjfricke/go-nginx`
+	- Make sure `$GOPATH/bin` is in your path
+
+## Usage
+
+```
+	
+```
--- go-nginx.go
+++ go-nginx.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	// "io/ioutil"
-	"log"
+	"flag"
+	"fmt"
 	// "os"
+)
+
+var (
+	path     = flag.String("path", "/var/www", "path to where to save directory")
+	domain   = flag.String("domain", "", "domain of site")
+	suffix   = flag.String("suffix", "html", "folder to append inside your directory for files")
+	input    = flag.String("input", "", "input files with line-by-line list of flags for batch run")
+	cDefault = flag.Bool("default", "false", "creates a default file instead")
+
+	available string = "/etc/nginx/sites-available"
+	enabled   string = "/etc/nginx/sites-enabled"
+	fullPath  string
 )
 
 func checkErr(err error) {
@@ -12,6 +25,76 @@ func checkErr(err error) {
 	}
 }
 
+// Default returns configuration for default site
+func Default() string {
+	return fmt.Sprintf(`
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root %s;
+    index index.html;
+
+    server_name _;
+
+    location / {
+            try_files $uri $uri/ =404;
+    }
+}`, fullPath)
+}
+
+// Config returns configuration for normal site
+func Config() string {
+	return fmt.Sprintf(`
+# For HTTPS Support
+#server {
+#       listen 80;
+#       listen [::]:80;
+#       server_name %s www.%s;
+#       return 301 https://$server_name$request_uri;
+#}
+
+server {
+
+        listen 80;
+        listen [::]:80;
+
+#       listen 443 ssl http2;
+#       listen [::]:443 ssl http2;
+
+#       ssl_certificate /etc/letsencrypt/live/%s/fullchain.pem;
+#       ssl_certificate_key /etc/letsencrypt/live/%s/privkey.pem;
+#       include snippets/ssl-%s.conf;
+#       include snippets/ssl-params.conf;
+
+        root %s;
+        index index.html;
+
+        server_name %s www.%s;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+
+#        location ~ /.well-known {
+#                allow all;
+#        }
+}`, *domain, *domain, *domain, *domain, *domain, fullPath, *domain, *domain)
+}
+
 func main() {
-	log.Println("init")
+
+	flag.Parse()
+
+	if *domain == "" || *path == "" {
+		panic("Domain or path is nil")
+	}
+
+	fullPath = fmt.Sprintf("%s/%s/%s", *path, *domain, *suffix)
+
+	if *cDefault {
+		fmt.Println(Default())
+	} else {
+		fmt.Println(Config())
+	}
 }
